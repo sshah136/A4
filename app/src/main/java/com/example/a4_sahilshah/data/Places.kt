20b2bb81fc39d9538e7d52440cdf78e647c41f85
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-08-14 15:55:58 -0400

    Data Class

---
+++ app/src/main/java/com/example/a4_sahilshah/data/Places.kt
@@ -0,0 +1,10 @@
+package com.example.a4_sahilshah.data
+
+import com.google.android.gms.maps.model.LatLng
+
+data class Place(
+    val img: Int,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double
+)
